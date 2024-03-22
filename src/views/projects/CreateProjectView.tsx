d81fdea9d19b5e4612c793c08292e4e2a164d565
iachb <ivanchavezbrb@gmail.com>
2024-03-22 12:26:58 -0600

    Creating the create project component

--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,18 +1,18 @@
 import { Link } from "react-router-dom";
 
-export default function DashboardView() {
+export default function CreateProjectView() {
   return (
     <>
-      <h1 className="text-5xl font-black">My Projects</h1>
+      <h1 className="text-5xl font-black">Create Project</h1>
       <p className="text-2xl font-light text-gray-500 mt-5">
-        Manage your projects
+        Fill out the following form to create a project
       </p>
-      <nav className='my-5'>
+      <nav className="my-5">
         <Link
-          to={"/projects/create"}
+          to={"/"}
           className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
         >
-          New Project
+          Go back to Projects
         </Link>
       </nav>
     </>
