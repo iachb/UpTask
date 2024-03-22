709be898df7ebfc8248bddccc21e04058c863129
iachb <ivanchavezbrb@gmail.com>
2024-03-22 12:38:18 -0600

    Adding component to create projects

--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,20 +1,51 @@
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 export default function CreateProjectView() {
+  const initialValues = {
+    projectName: "",
+    clienName: "",
+    description: "",
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ defaultValues: { initialValues } });
+
+  const handleForm = (data) => {
+    console.log(data);
+  };
+
   return (
     <>
-      <h1 className="text-5xl font-black">Create Project</h1>
-      <p className="text-2xl font-light text-gray-500 mt-5">
-        Fill out the following form to create a project
-      </p>
-      <nav className="my-5">
-        <Link
-          to={"/"}
-          className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+      <div className="max-w-3xl mx-auto">
+        <h1 className="text-5xl font-black">Create Project</h1>
+        <p className="text-2xl font-light text-gray-500 mt-5">
+          Fill out the following form to create a project
+        </p>
+        <nav className="my-5">
+          <Link
+            to={"/"}
+            className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+          >
+            Go back to Projects
+          </Link>
+        </nav>
+        <form
+          className="mt-10 bg-white shadow-lg p-10 rounded-lg"
+          onSubmit={handleSubmit(handleForm)}
+          noValidate
         >
-          Go back to Projects
-        </Link>
-      </nav>
+          <input
+            type="submit"
+            value="Create Proyect"
+            className="bg-fuchsia-600 hover:bg-fuchsia-700 w-full p-3 
+          text-white uppercase font-bold cursor-pointer transition-colors"
+          />
+        </form>
+      </div>
     </>
   );
 }
