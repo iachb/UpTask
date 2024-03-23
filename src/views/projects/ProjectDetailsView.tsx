25cce5653b38c993f60bf63e24a163a1339ed3ff
iachb <ivanchavezbrb@gmail.com>
2024-03-23 16:26:26 -0600

    Creating route to Add Tasks and creating component

---
+++ src/views/projects/ProjectDetailsView.tsx
@@ -0,0 +1,47 @@
+import { Navigate, useNavigate, useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { getProjectbyId } from "@/api/ProjectAPI";
+import EditProjectForm from "@/components/projects/EditProjectForm";
+import AddTaskModal from "@/components/tasks/AddTaskModal";
+
+export default function ProjectDetailsView() {
+  const navigate = useNavigate();
+  // Get the projectId from the URL
+  const params = useParams();
+  const projectId = params.projectId!;
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["editProject", projectId],
+    queryFn: () => getProjectbyId(projectId),
+    retry: false,
+  });
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (isError) {
+    return <Navigate to="/404" />;
+  }
+
+  if (data)
+    return (
+      <>
+        <h1 className="text-5xl font-black">{data.projectName}</h1>
+        <p className="text-2xl font-light text-gray-500 mt-5">
+          {data.description}
+        </p>
+        <nav className="my-5 gap-3">
+          <button
+            type="button"
+            className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+            onClick={() => navigate("?newTask=true")}
+          >
+            Add Task
+          </button>
+        </nav>
+
+        <AddTaskModal />
+      </>
+    );
+}
