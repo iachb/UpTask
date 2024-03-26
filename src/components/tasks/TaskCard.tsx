8a4f9b48b13532755daf9101493b0a18ca348541
iachb <ivanchavezbrb@gmail.com>
2024-03-25 22:04:04 -0600

    invalidate editProject query

--- src/components/tasks/TaskCard.tsx
+++ src/components/tasks/TaskCard.tsx
@@ -1,8 +1,11 @@
 import { Fragment } from "react";
-import { Task } from "@/types/index";
 import { Menu, Transition } from "@headlessui/react";
 import { EllipsisVerticalIcon } from "@heroicons/react/20/solid";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import {useMutation, useQueryClient} from "@tanstack/react-query";
+import { toast } from "react-toastify";
+import { deleteTask } from "@/api/TasksAPI";
+import { Task } from "@/types/index";
 
 type TaskCardProps = {
   task: Task;
@@ -11,6 +14,25 @@ type TaskCardProps = {
 export default function TaskCard({ task }: TaskCardProps) {
 
   const navigate = useNavigate();
+
+  // Get the project ID from the URL
+  const params = useParams();
+  const projectId = params.projectId!;
+
+  const queryClient = useQueryClient();
+
+
+  const {mutate} = useMutation({
+    mutationFn: deleteTask,
+    onError: (error: Error) => {
+      toast.error(error.message);
+    },
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ["editProject", projectId] });
+      toast.success(data);
+      navigate(location.pathname, { replace: true });
+    },
+  });
 
   return (
     <li className="p-5 bg-white border border-slate-300 flex justify-between gap-3">
@@ -61,6 +83,7 @@ export default function TaskCard({ task }: TaskCardProps) {
                 <button
                   type="button"
                   className="block px-3 py-1 text-sm leading-6 text-red-500"
+                  onClick={() => mutate({projectId, taskId: task._id})}
                 >
                   Delete Task
                 </button>
