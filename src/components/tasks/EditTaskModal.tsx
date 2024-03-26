c07cfcb7548e99c05d8ba52711a10e8ab3fe1d06
iachb <ivanchavezbrb@gmail.com>
2024-03-26 13:09:43 -0600

    Invalidate cache on edit task API

--- src/components/tasks/EditTaskModal.tsx
+++ src/components/tasks/EditTaskModal.tsx
@@ -36,7 +36,8 @@ export default function EditTaskModal({ data, taskId }: EditTaskModalProps) {
       toast.error(error.message);
     },
     onSuccess: (data) => {
-      queryClient.invalidateQueries({ queryKey: ["project", projectId] });
+      queryClient.invalidateQueries({ queryKey: ["project", projectId] })
+      queryClient.invalidateQueries({ queryKey: ["task", taskId] });
       toast.success(data);
       reset();
       navigate(location.pathname, { replace: true });
