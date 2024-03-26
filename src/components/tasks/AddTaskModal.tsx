c1d5ce6c9fa5f41a55de0796aa28f7814b673681
iachb <ivanchavezbrb@gmail.com>
2024-03-25 20:01:58 -0600

    Invalidating task queries

--- src/components/tasks/AddTaskModal.tsx
+++ src/components/tasks/AddTaskModal.tsx
@@ -2,7 +2,7 @@ import { Fragment } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { useForm } from "react-hook-form";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import TaskForm from "./TaskForm";
 import { TaskFormData } from "@/types/index";
 import { createTask } from "@/api/TasksAPI";
@@ -32,12 +32,15 @@ export default function AddTaskModal() {
     formState: { errors },
   } = useForm({ defaultValues: initialValues });
 
+  const queryClient = useQueryClient();
+
   const { mutate } = useMutation({
     mutationFn: createTask,
     onError: (error) => {
       toast.error(error.message);
     },
     onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ["editProject", projectId] });
       toast.success(data);
       navigate(location.pathname, { replace: true });
       reset();
